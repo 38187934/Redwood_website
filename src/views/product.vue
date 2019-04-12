14f6f1b085e648341b4c82d82898efda3d996555
clarence <1057697328@qq.com>
2019-04-12 13:18:20 +0800

    完成产品展示API调用。

--- src/views/product.vue
+++ src/views/product.vue
@@ -19,7 +19,7 @@
 
       <div class="column">
         <div class="container">
-          您现在的位置： 首页>关于科义 > 产品中心
+          您现在的位置： 首页>关于科义>{{breadName}}
         </div>
       </div>
 
@@ -35,28 +35,15 @@
             <p style="font-size:0.9rem">PRODUCT CENTER</p>
           </div>
 
-          <div class="columns">
+          <!--左侧类型菜单-->
+          <div class="columns is-multiline">
             <div class="column is-12">
-              <div class="menuTab active">
-                水产系列
+              <div class="menuTab" v-for="item in typeList" :class="item.goodsCategoryId===currentCategoryId?'active':''" @click="changeTab(item.goodsCategoryId,item.goodsCategoryName)">
+                {{item.goodsCategoryName}}
               </div>
             </div>
-          </div>
 
-          <div class="columns">
-            <div class="column is-12">
-              <div class="menuTab">
-                什么系列
-              </div>
-            </div>
-          </div>
 
-          <div class="columns">
-            <div class="column is-12">
-              <div class="menuTab">
-                什么系列
-              </div>
-            </div>
           </div>
 
 
@@ -67,16 +54,16 @@
           <div class="infoPanel">
             <div class="columns">
               <div class="column">
-                <h2 class="title">水产系列</h2>
+                <h2 class="title">{{breadName}}</h2>
               </div>
               <div class="column">
 
                 <div class="columns has-text-right is-desktop is-mobile">
                   <div class="column is-6-desktop is-9-mobile is-offset-4-desktop">
-                    <input class="input is-rounded" type="text" placeholder="请输入产品名称">
+                    <input class="input is-rounded" type="text" placeholder="请输入产品名称" v-model="keyword">
                   </div>
                   <div class="column is-2-desktop is-3-mobile">
-                    <a class="button is-info is-rounded">搜索</a>
+                    <a class="button is-info is-rounded" @click="search()">搜索</a>
                   </div>
                 </div>
               </div>
@@ -86,93 +73,23 @@
 
 
             <div class="columns is-multiline">
-              <div class="column is-4">
+              <div class="column is-4" v-for="item in productPage.records" @click="getDetail(item)" style="cursor: pointer">
 
                 <div class="card">
                   <div class="card-image">
                     <figure class="image is-4by3">
-                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                      <img :src="item.images!=null?JSON.parse(item.images)[0]:''" :alt="item.goodsName">
                     </figure>
                   </div>
                   <div class="card-content">
-                    <h2 class="title">产品名称</h2>
+                    <h2 class="title">{{item.goodsName}}</h2>
                   </div>
                 </div>
 
               </div>
 
 
-              <div class="column is-4">
 
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <h2 class="title">产品名称</h2>
-                  </div>
-                </div>
-
-              </div>
-              <div class="column is-4">
-
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <h2 class="title">产品名称</h2>
-                  </div>
-                </div>
-
-              </div>
-              <div class="column is-4">
-
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <h2 class="title">产品名称</h2>
-                  </div>
-                </div>
-
-              </div>
-              <div class="column is-4" @click="showModal=true">
-
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <h2 class="title">产品名称</h2>
-                  </div>
-                </div>
-
-              </div>
-
-              <div class="column is-4">
-
-                <div class="card">
-                  <div class="card-image">
-                    <figure class="image is-4by3">
-                      <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
-                    </figure>
-                  </div>
-                  <div class="card-content">
-                    <h2 class="title">产品名称</h2>
-                  </div>
-                </div>
-
-              </div>
 
             </div>
 
@@ -202,13 +119,10 @@
 
           <div class="columns is-multiline">
             <div class="column is-4 has-text-centered">
-              <img style="border:2px solid #0088c7;"  src="http://via.placeholder.com/260x350?text=250X350" alt="">
+              <img style="border:2px solid #0088c7;" :src="productDetail.images!=null?JSON.parse(productDetail.images)[0]:''" alt="">
             </div>
             <div class="column is-8 modal_detail">
-              <p class="descText">【主要成分】益母草、当归、川芎、桃仁、炮姜等。</p>
-              <p class="descText">【性 状】本品为黄绿色的粉末；气清香，味甘、微苦。</p>
-              <p class="descText">【功 能】活血祛淤，温经止痛。</p>
-              <p class="descText">【主 治】产后恶露不行，血瘀腹痛。</p>
+              <p class="descText">{{productDetail.goodsName}}</p>
             </div>
           </div>
 
@@ -222,13 +136,123 @@
 <script>
   import Header from "../components/header";
   import Footer from "../components/footer";
+  //引入常量文件
+  import CONSTANT from '../assets/constant';
 
   export default {
-    name: "about",
+    name: "product",
     components: {Footer, Header},
     data() {
       return {
-        showModal: false
+        showModal: false,
+        breadName:null,
+        //产品类型列表
+        typeList:null,
+        //产品列表分页
+        productPage:{
+          records:[]
+        },
+        //商品编号
+        goodsId:null,
+        //当前选中类型ID
+        currentCategoryId:null,
+        //商品详情
+        productDetail:{
+          goodsName:null,
+          images:null
+        },
+        //模糊查询产品名称
+        keyword:null
+      }
+    },
+    mounted() {
+      this.getGoodsCategories();
+    },
+    methods:{
+      /**
+       * 获取产品类型列表
+       */
+      getGoodsCategories(){
+        this.axios.get(CONSTANT.baseURL+"/pc/goods/categories")
+          .then((json)=>{
+            if(json.data.code!==CONSTANT.statusCode.SUCCESS)
+            {
+              CONSTANT.failedAlert('提示',json.data.msg);
+              return false;
+            }
+            else
+            {
+              this.typeList=json.data.list;
+              this.currentCategoryId=json.data.list[0].goodsCategoryId;
+              this.breadName=json.data.list[0].goodsCategoryName;
+              this.getProductPage(this.currentCategoryId);
+            }
+          })
+      },
+      /**
+       * 通过产品类型ID&产品名称分页获取产品列表
+       * @param categoryId
+       */
+      getProductPage(categoryId,name)
+      {
+        let basePath = "/pc/goods/search?categoryId="+categoryId;
+        if(name!==undefined)
+        {
+          basePath+="&name="+name
+        }
+        this.axios.get(CONSTANT.baseURL+basePath)
+          .then((json)=>{
+            if(json.data.code!==CONSTANT.statusCode.SUCCESS)
+            {
+              CONSTANT.failedAlert('提示',json.data.msg);
+              return false;
+            }
+            else
+            {
+              this.productPage=json.data.page;
+              /*
+
+              //图片数组处理
+              this.productPage.records.forEach((obj,i)=>{
+                //去除反斜杠
+                let reg = new RegExp("\\\\","g");
+                obj.images=obj.images.replace(reg,"\\\\");
+                //去除数组左右边[]
+                obj.images=obj.images.replace("[","").replace("]","").replace(" ","");
+                let reg2 = new RegExp('"',"g");
+                //去除双引号
+                obj.images=obj.images.replace(reg2,"")
+                obj.images=obj.images.split(",");
+              });
+
+               */
+
+            }
+          })
+      },
+      /**
+       * 切换板块
+       * @param categoryId
+       */
+      changeTab(categoryId,breadName)
+      {
+        this.getProductPage(categoryId);
+        this.currentCategoryId=categoryId;
+        this.breadName=breadName;
+
+      },
+      //展示商品详情模态框
+      getDetail(obj)
+      {
+        this.productDetail=obj;
+        this.showModal=true;
+      },
+      /**
+       * 模糊查询，按类别ID以及模糊名称
+       */
+      search()
+      {
+        this.getProductPage(this.currentCategoryId,this.keyword);
       }
     }
   }
@@ -236,11 +260,9 @@
 
 <style scoped>
 
-  @media screen and (min-width: 769px)
-  {
-    .modal-card
-    {
-      width:800px;
+  @media screen and (min-width: 769px) {
+    .modal-card {
+      width: 800px;
     }
   }
 
@@ -301,14 +323,14 @@
     font-size: 1.2rem;
     font-weight: normal;
   }
+
   /**
     模态框内产品简介文字样式
    */
-  .modal_detail .descText
-  {
-    font-size:1.2rem;
-    margin-bottom:0.5rem;
-    color:#0088c7;
+  .modal_detail .descText {
+    font-size: 1.2rem;
+    margin-bottom: 0.5rem;
+    color: #0088c7;
   }
 
 </style>
